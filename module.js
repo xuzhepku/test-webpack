ebaf2dd1ac9f6fe3ebacbf34ab2f3260d55677b5
JasonHsu <xuzhepku@gmail.com>
2016-10-10 12:16:29 +0800

    First commit

---
+++ module.js
@@ -0,0 +1,4 @@
+/**
+ * Created by jason on 10/10/2016.
+ */
+module.exports = 'It works from module.js.'
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+/**
+ * Created by jason on 10/10/2016.
+ */
+var webpack = require('webpack')
+
+module.exports = {
+    entry  : './entry.js',
+    output : {
+        path     : __dirname,
+        filename : 'bundle.js'
+    },
+    module : {
+        loaders : [
+            {test : /\.css$/, loader : 'style!css'}
+        ],
+        plugins : [
+            new webpack.BannerPlugin('This file is created by zhaoda')
+        ]
+    }
+}
